d7fe5a2805760e5053b042af76682a66f0ed547d
Junior Plays <50379079+Junior1Plays@users.noreply.github.com>
2021-11-23 23:56:17 -0300

    release 1.1
    v1.1 is released! 😁

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ token = input("Insira seu Token (para o Discord autorizar a mensagem): ")
 
 channel_id = input("\nInsira o ID do Canal: ")
 
+sends = 0
 
 print("\nEnviando...\n")
 
@@ -22,11 +23,18 @@ header = {
     "authorization": token
 }
 
-def sendppgrow(tkn, cid):
-    print("Aguardando 10 segundos...\n\n")
-    time.sleep(10)
-    r = requests.post(url, headers=header, data=payload)
-    print("Mensagem enviada!\n")
-    return sendppgrow(token, channel_id)
+def sendppgrow(tkn, cid, snds):
+    if(snds == 150):
+        print("Aguardando 30 segundos...\n\n")
+        time.sleep(30000)
+        sends = 0
+    else:
+        print("Aguardando 10 segundos...\n")
+        time.sleep(10)
+        r = requests.post(url, headers=header, data=payload)
+        sends = snds + 1
+        print(f"{snds} mensagens enviadas...\n")
+        print("Mensagem enviada!\n")
+        return sendppgrow(token, channel_id, sends)
 
-sendppgrow(token, channel_id)
+sendppgrow(token, channel_id, sends)
