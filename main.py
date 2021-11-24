b2d696e857f67f7a17c6a2f8d67aa5da4bcfc172
Junior Plays <50379079+Junior1Plays@users.noreply.github.com>
2021-11-24 00:18:49 -0300

    update 1.1

--- main.py
+++ main.py
@@ -23,18 +23,19 @@ header = {
     "authorization": token
 }
 
-def sendppgrow(tkn, cid, snds):
+def sendppgrow(snds):
     if(snds == 150):
         print("Aguardando 30 segundos...\n\n")
-        time.sleep(30000)
-        sends = 0
+        time.sleep(30)
+        snds = 0
+        return sendppgrow(0)
     else:
         print("Aguardando 10 segundos...\n")
         time.sleep(10)
         r = requests.post(url, headers=header, data=payload)
         sends = snds + 1
-        print(f"{snds} mensagens enviadas...\n")
+        print(f"{snds + 1} mensagens enviadas...\n")
         print("Mensagem enviada!\n")
-        return sendppgrow(token, channel_id, sends)
+        return sendppgrow(sends)
 
-sendppgrow(token, channel_id, sends)
+sendppgrow(sends)
